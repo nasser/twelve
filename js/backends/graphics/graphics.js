10dce7b80924d70fde48129873be5e63808e30f4
Ramsey Nasser <ram@nas.sr>
2013-02-21 03:19:09 -0500

    Add key* functions to Graphics backend

--- js/backends/graphics/graphics.js
+++ js/backends/graphics/graphics.js
@@ -1,2 +1,14 @@
 document.write("<canvas>")
 var Graphics = new Processing(document.querySelector('canvas'));
+
+Graphics.keyPressed = function() {
+  World.query("keyboard").forEach(function(e) {
+    e.key = Graphics.keyCode;
+  });
+}
+
+Graphics.keyReleased = function() {
+  World.query("keyboard").forEach(function(e) {
+    delete e.key;
+  });
+}
